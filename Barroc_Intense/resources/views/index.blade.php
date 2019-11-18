34cb447785276f15945395f8303d5a7c5e73550c
Dani <d.kools1@outlook.com>
2019-11-18 14:01:20 +0100

    Added submit button in contact form

--- Barroc_Intense/resources/views/index.blade.php
+++ Barroc_Intense/resources/views/index.blade.php
@@ -2,22 +2,26 @@
             @csrf
             <div class="form-group">
                 <label for="name">Naam</label>
-                <input class="form-control" type="text" name="name">
+                <input type="text" name="name">
             </div>
 
             <div class="form-group">
                 <label for="email">E-mail</label>
-                <input class="form-control" type="email" name="email">
+                <input type="email" name="email">
             </div>
 
             <div class="form-group">
                 <label for="topic">Onderwerp</label>
-                <input class="form-control" type="text" name="topic">
+                <input type="text" name="topic">
             </div>
 
             <div class="form-group">
                 <label for="description">Beschrijving</label>
-                <input class="form-control" type="text" name="description">
+                <input type="text" name="description">
+            </div>
+
+            <div class="form-group">
+                <input type="submit" value="Vraag offerte aan">
             </div>
         </form>
 
