2206063b27e9de50b7854f3ccc73943000f6b964
Dani <d.kools1@outlook.com>
2019-11-25 14:02:49 +0100

    Offer template toegevoegd

---
+++ Barroc_Intense/resources/views/email_template_offer_blade.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Dani Kools
+ * Date: 25-11-2019
+ * Time: 13:48
+ */
+
+// klantnaam, 
---
+++ Barroc_Intense/resources/views/offer/create.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts/app')
+
+@section('content')
+    <h1>Maak Prijsopgace</h1>
+
+    <form action="" method="post">
+        @csrf
+        .form-group
+    </form>
+@endsection
