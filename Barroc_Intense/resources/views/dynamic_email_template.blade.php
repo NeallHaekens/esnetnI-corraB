35a91e7e17894b4a985713eea0dca291359c38ee
Dani <d.kools1@outlook.com>
2019-11-25 13:06:36 +0100

    Mail template veranderd

--- Barroc_Intense/resources/views/dynamic_email_template.blade.php
+++ Barroc_Intense/resources/views/dynamic_email_template.blade.php
@@ -1,4 +1,5 @@
-<p>Hi , This is {{ $data['name'] }}</p>
-<p>I have some query like {{ $data['topic'] }}</p>
-<p>It would be appriciative, if you gone through this feedback.</p>
-<p>{{ $data['description'] }}</p>
+<p>Klant naam : {{ $data['name'] }}</p>
+<br>
+<p>Onderwerp : {{ $data['topic'] }}</p>
+<br>
+<p>Beschrijving : {{ $data['beschrijving'] }}</p>
