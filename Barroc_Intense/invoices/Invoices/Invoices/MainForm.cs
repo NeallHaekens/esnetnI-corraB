9b4a20e28e98a75238f133ae17132d307b26322e
J.M.K Tax <d178453@edu.rocwb.nl>
2019-12-03 16:43:43 +0100

    Bezig met Contracten en Facturen

---
+++ Barroc_Intense/invoices/Invoices/Invoices/MainForm.cs
@@ -0,0 +1,70 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Invoices
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void CreateInvoiceToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void CheckInvoicesToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void NameTextBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void DateTimePicker1_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void StreetTextBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void CityTextBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ProvinceTextBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void DescriptionTextBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ExitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void LogoPictureBox_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
